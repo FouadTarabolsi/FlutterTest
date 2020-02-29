2bb8428b4ccc08299ec604f2ac9ee6158ea167c5
FouadTarabolsi <fx008@yahoo.com>
2020-02-29 20:58:22 +0300

    step 4 of the codelab

--- secondrun/lib/main.dart
+++ secondrun/lib/main.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,13 +14,46 @@ class MyApp extends StatelessWidget {
         title: "Welcome to Flutter",
         home: Scaffold(
           appBar: AppBar(
-            title: Text("Welcome TO Flutter",),
+            title: Text(
+              "Welcome To Flutter",
+            ),
           ),
           body: Center(
-            child: Text("Hello World"),
-            
+            child: RandomWords(),
           ),
-          
         ));
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final _suggestions = <WordPair>[];
+    final _biggerFont = const TextStyle(fontSize: 18.0);
+
+    final wordPair = WordPair.random();
+    return Text(
+      wordPair.asPascalCase,
+      textScaleFactor: 2.5,
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
