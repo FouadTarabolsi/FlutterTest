1aada6e45d30a29dba5cd3b7205b4a6c68502af7
FouadTarabolsi <fx008@yahoo.com>
2020-03-04 19:14:03 +0300

    finished codelab 2

--- secondrun/lib/main.dart
+++ secondrun/lib/main.dart
@@ -11,47 +11,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Welcome to Flutter",
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "Welcome To Flutter",
-            ),
-          ),
-          body: Center(
-            child: RandomWords(),
-          ),
-        ));
+        title: "Startup Name Generator",
+        home: RandomWords(),
+    );
+        
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont  = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     final _suggestions = <WordPair>[];
     final _biggerFont = const TextStyle(fontSize: 18.0);
 
     final wordPair = WordPair.random();
-    return Text(
-      wordPair.asPascalCase,
-      textScaleFactor: 2.5,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+    
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return _buildRow(_suggestions[index]);
+
+
+    });
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
     );
   }
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
 
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
   }
-}
+
 
 class RandomWords extends StatefulWidget {
   @override
